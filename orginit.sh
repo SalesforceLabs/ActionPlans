f9014f2d2f0a68011ab30b07a09b59149d7639c0
dschach <dschach@x2od.com>
2021-05-20 20:08:35 -0700

    orginit update

--- orginit.sh
+++ orginit.sh
@@ -11,4 +11,4 @@ sfdx force:org:open
 
 #Deploy to ActionPlans Dev/Packaging Org: Use -c to check only first
 #
-#sfdx force:source:deploy -u ActionPlansDevOrg -w 100 -p force-app/main/default
+#sfdx force:source:deploy -u ActionPlansPackagingOrg -w 100 -p force-app/main/default
